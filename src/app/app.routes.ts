b4b11253e921e6c63abdff77918d5dc44ce04036
Johan Magnusson <joma2304@student.miun.se>
2024-04-17 17:37:45 +0200

    Lagt till componen för meny samt styling för den

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConvertComponent } from './convert/convert.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'convert', component: ConvertComponent},
     { path: 'about', component: AboutComponent}, 
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '404', component: NotFoundComponent}, 
+    { path: '**', component: NotFoundComponent}
 ];
